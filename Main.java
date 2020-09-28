f6ce3d2c7a79271f2339c706177df94949ec431a
manuel1410 <71191544+manuel1410@users.noreply.github.com>
2020-09-27 20:13:49 -0600

    Update Main.java
    añadí unas pruebas de la clase items

--- Main.java
+++ Main.java
@@ -9,9 +9,33 @@ package com.mycompany.tienda;
  *
  * @author Andrey Sancho
  */
+
+import.util.List;
+import.util.ArrayList;
+import.items; //Aquí pretendo importar el archivo items.java
+
 public class Main {
     public static void main(String[] args){
         System.out.println("welcome to the shop");
+
+
+
+        ArrayList<items> listaitemsatq = new ArrayList<items>();
+
+        item item01_atq = new item(50,0,5,0,5);
+        item item02_atq = new item(75,0,10,5,0);
+        item item03_atq = new item(85,10,25,0,0);
+        item item04_atq = new item(100,20,0,0,0);
+        item item05_atq = new item(105,0,15,25,5);
+
+
+        ArrayList<item> listaitemsdef = new ArrayLista<items>();
+
+        item item01_def = new item(0,75,0,0,5);
+        item item02_def = new item(0,100,0,0,10);
+        item item03_def = new item(0,105,0,25,0);
+        item item04_def = new item(0,125,0,0,40);
+        item item05_def = new item(0,85,0,25,50);
         
     }
     
